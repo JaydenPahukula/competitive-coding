608f7b5b42f940b8c82c187ac38f80c74825530d
Jayden Pahukula <72293021+JaydenPahukula@users.noreply.github.com>
2023-08-25 10:44:35 -0600

    .

--- Kattis/simplearithmetic.py
+++ Kattis/simplearithmetic.py
@@ -1,25 +1,3 @@
-m = int(input())
-w = 0
-h = 0
-currw = 0
-currh = 0
-while 1:
-    inp = list(map(int, input().split()))
-    if inp == [-1, -1]:
-        print(f"{w} x {h}")
-        m = int(input())
-        if m == 0: break
-        w = 0
-        h = 0
-        currw = 0
-        currh = 0
-    else:
-        w1, h1 = inp
-        if w1 + currw > m:
-            currw = w1
-            currh = h
-            h += h1
-        else:
-            currw += w1
-            w = max(w, currw)
-            h = max(h, currh+h1)
+from decimal import Decimal
+a, b, c = map(Decimal, input().split())
+print(a*b/c)

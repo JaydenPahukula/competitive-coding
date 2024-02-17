9cdcd298fbe50adf9b9fa449770423f329b97f22
Jayden Pahukula <jayden.pahukula@gmail.com>
2024-02-16 17:39:03 -0700

    .

--- Kattis/carefulascent.py
+++ Kattis/carefulascent.py
@@ -1,9 +1,15 @@
 x, y = map(int, input().split())
 n = int(input())
 
-for _ in range(n):
-    start, stop, factor = map(float, input().split())
-    dist = stop-start
-    y = y - dist + (dist * factor)
+# for _ in range(n):
+#     start, stop, factor = map(float, input().split())
+#     dist = stop-start
+#     y = y - dist + (dist * factor)
 
-print(x/y)
+# print(x/y)
+
+sheilds = [tuple(map(float, input().split())), ]
+
+lo, hi = 0,99**9
+for _ in range(1000):
+    m = (hi+lo)/2
